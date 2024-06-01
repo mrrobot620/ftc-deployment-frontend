97de59f78333d3fedf3faa0c1325fe5c39a2b202
Abhishek Rajput <abhishek620380@gmail.com>
2024-06-01 23:02:33 +0530

    Added Mobile SideBar

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import Link from "next/link";
 import { LucideIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import {
@@ -13,7 +10,7 @@ import {
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { usePathname } from "next/navigation";
 
-import { SheetClose } from "@/components/ui/sheet"
+import { SheetClose } from "@/components/ui/sheet";
 
 interface NavProps {
   isCollapsed: boolean;
@@ -35,25 +32,25 @@ export function Nav({ links, isCollapsed }: NavProps) {
         className="group flex flex-col gap-10 py-6 data-[collapsed=true]:py-2"
       >
         <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center pr-6 group-[[data-collapsed=true]]:px-2">
-          {links.map((link, index) =>
+          {links.map((link) =>
             isCollapsed ? (
-              <Tooltip key={index} delayDuration={0}>
+              <Tooltip key={link.href} delayDuration={0}>
                 <TooltipTrigger asChild>
-                    <Link
-                      href={link.href}
-                      className={cn(
-                        buttonVariants({
-                          variant: link.href === pathname ? "default" : "ghost",
-                          size: "icon",
-                        }),
-                        "h-9 w-9",
-                        link.variant === "default" &&
-                          "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-                      )}
-                    >
-                      <link.icon className="h-4 w-4" />
-                      <span className="sr-only">{link.title}</span>
-                    </Link>
+                  <Link
+                    href={link.href}
+                    className={cn(
+                      buttonVariants({
+                        variant: link.href === pathname ? "default" : "ghost",
+                        size: "icon",
+                      }),
+                      "h-9 w-9",
+                      link.variant === "default" &&
+                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                    )}
+                  >
+                    <link.icon className="h-4 w-4" />
+                    <span className="sr-only">{link.title}</span>
+                  </Link>
                 </TooltipTrigger>
                 <TooltipContent side="right" className="flex items-center gap-4">
                   {link.title}
@@ -65,34 +62,33 @@ export function Nav({ links, isCollapsed }: NavProps) {
                 </TooltipContent>
               </Tooltip>
             ) : (
-                <Link
-                  href={link.href}
-                  className={cn(
-                    buttonVariants({
-                      variant: link.href === pathname ? "default" : "ghost",
-                      size: "sm",
-                    }),
-                    link.variant === "default" &&
-                      "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-                    "justify-start"
-                  )}
-                > 
-                  <link.icon className="mr-2 h-4 w-4" />
-                  {link.title}
-                  {link.label && (
-
-                    <span
-                      className={cn(
-                        "ml-auto",
-                        link.variant === "default" &&
-                          "text-background dark:text-white"
-                      )}
-                    >
-
-                      {link.label}
-                    </span>
-                  )}
-                </Link>
+              <Link
+                key={link.href}
+                href={link.href}
+                className={cn(
+                  buttonVariants({
+                    variant: link.href === pathname ? "default" : "ghost",
+                    size: "sm",
+                  }),
+                  link.variant === "default" &&
+                    "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                  "justify-start"
+                )}
+              >
+                <link.icon className="mr-2 h-4 w-4" />
+                {link.title}
+                {link.label && (
+                  <span
+                    className={cn(
+                      "ml-auto",
+                      link.variant === "default" &&
+                        "text-background dark:text-white"
+                    )}
+                  >
+                    {link.label}
+                  </span>
+                )}
+              </Link>
             )
           )}
         </nav>
